383e7deb3aa7e09b2e721912b32482e9638a939c
rayanefkarolina <rayane.karolina@a.ucb.br>
2024-09-04 18:49:43 -0300

    programa concluído

---
+++ Calcularsoma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Calcularsoma
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int INDICE = 13, SOMA = 0, K = 0;
+
+      while (K < INDICE)
+      {
+        K = K + 1;
+        SOMA = SOMA + K;
+      }
+
+      Console.WriteLine("O valor final de SOMA é: " + SOMA);
+    }
+
+  }
+
+}
