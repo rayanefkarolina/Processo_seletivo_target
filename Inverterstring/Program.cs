b86e7a551d4dd45e1c5a40e4332d46d92e3e0f5e
rayanefkarolina <rayane.karolina@a.ucb.br>
2024-09-05 19:41:53 -0300

    programa 5 concluido

--- Inverterstring/Program.cs
+++ Inverterstring/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Informe uma string para ser invertida:");
+    string? input = Console.ReadLine();
+
+    if (input == null)
+    {
+      Console.WriteLine("Entrada inválida. Nenhuma string fornecida.");
+    }
+    else
+    {
+      string reversed = ReverseString(input);
+      Console.WriteLine("String invertida: " + reversed);
+    }
+  }
+
+  static string ReverseString(string str)
+  {
+
+    if (str == null)
+    {
+      throw new ArgumentNullException(nameof(str), "O argumento não pode ser nulo.");
+    }
+
+    char[] charArray = new char[str.Length];
+    int end = str.Length - 1;
+
+
+    for (int i = 0; i < str.Length; i++)
+    {
+      charArray[i] = str[end - i];
+    }
+
+    return new string(charArray);
+  }
+}
